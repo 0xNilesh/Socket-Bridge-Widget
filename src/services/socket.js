4ae77a753d4ba7519b4d915bd9967b530641afd0
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-04-16 17:13:08 +0530

    Added api-key in headers

--- src/services/socket.js
+++ src/services/socket.js
@@ -4,14 +4,17 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const socketApi = createApi({
   reducerPath: 'socketApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://backend.movr.network/v2/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://backend.movr.network/v2/',
+    prepareHeaders: (headers) => {
+      headers.set('API-KEY', process.env.REACT_APP_API_KEY);
+      return headers;
+    }
+  }),
   endpoints: (builder) => ({
     getSupportedChains: builder.query({
       query: () => ({
         url: `supported/chains`,
-        headers: {
-          'API-KEY': '645b2c8c-5825-4930-baf3-d9b997fcd88c',
-        }
       })
     }),
   }),
