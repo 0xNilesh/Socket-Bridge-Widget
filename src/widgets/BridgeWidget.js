ba678649c509227b352a718a8be69200ea34c0d9
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-04-17 20:17:59 +0530

    Modified data result into more comfortable and useful data structure

--- src/widgets/BridgeWidget.js
+++ src/widgets/BridgeWidget.js
@@ -1,22 +1,37 @@
 import { setUserAddress} from "../redux/slice/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import React from "react";
+import "./BridgeWidget.css";
+
+import { useGetSupportedChainsQuery } from "../services/socket";
+
+const getChainDataByChainId = (chains) => {
+  if (!chains) return [];
+  const chainsByChainId = {}, chainsByNameToId = {};
+  chains.forEach((chain) => {
+    chainsByChainId[chain.chainId] = chain;
+    chainsByNameToId[chain.name] = chain.chainId;
+  });
+  return [ chainsByChainId, chainsByNameToId ];
+}
 
 const BridgeWidget = ({ address, provider }) => {
   const dispatch = useDispatch();
-  const { userAddress} = useSelector(state => state.user);
+  const chains = useGetSupportedChainsQuery();
+  const [chainsByChainId, chainsByNameToId ] = getChainDataByChainId(chains.data?.result);
+  console.log(chainsByChainId);
+  console.log(chainsByNameToId);
 
-  React.useEffect(() => {
-    if (!address) return;
-    dispatch(setUserAddress(address));
-  })
-
-  console.log(userAddress);
-  console.log(provider);
+  // React.useEffect(() => {
+  //   if (!address) return;
+  //   dispatch(setUserAddress(address));
+  // })
 
   return (
     <>
-      Hello BridgeWidget
+      <div className="sbw-outer-box">
+
+      </div>
     </>
   )
 }
