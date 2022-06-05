30993f8ba124b09ed5de77e72d03b1373bbb425d
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-06-06 00:17:19 +0530

    Added svgr rollup plugin to import svg files

--- rollup.config.js
+++ rollup.config.js
@@ -3,36 +3,38 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
+import svgr from "@svgr/rollup";
 
 const packageJson = require("./package.json");
 
 export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      postcss(),
-    ],
-    external: ['react','react-dom','axios', 'react-query']
-  },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: [/\.(css|less|scss)$/],
-  },
+	{
+		input: "src/index.ts",
+		output: [
+			{
+				file: packageJson.main,
+				format: "cjs",
+				sourcemap: true,
+			},
+			{
+				file: packageJson.module,
+				format: "esm",
+				sourcemap: true,
+			},
+		],
+		plugins: [
+			resolve(),
+			commonjs(),
+			typescript({ tsconfig: "./tsconfig.json" }),
+			postcss(),
+			svgr(),
+		],
+		external: ["react", "react-dom", "axios", "react-query"],
+	},
+	{
+		input: "dist/esm/types/index.d.ts",
+		output: [{ file: "dist/index.d.ts", format: "esm" }],
+		plugins: [dts()],
+		external: [/\.(css|less|scss)$/],
+	},
 ];
